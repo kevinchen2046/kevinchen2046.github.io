e9538918ebc0a96824c50a20a4d74e5cdbd794e2
nan.chen <kevin-chen@foxmail.com>
2022-11-03 14:15:08 +0800

    x
    x

---
+++ www/demo/Song-Maker/sources/src/grid/Percussion.js
@@ -0,0 +1,16 @@
+import {
+    AbstractInstrument
+} from './AbstractInstrument'
+import {
+    PercussionCanvasRenderer
+} from './PercussionCanvasRenderer'
+
+export class Percussion extends AbstractInstrument {
+    constructor(...args) {
+        super(...args, 'percussion-canvas')
+    }
+
+    rendererClass() {
+        return PercussionCanvasRenderer
+    }
+}
--- www/index.md
+++ www/index.md
@@ -1,1 +1,8 @@
-[Blog](blog/index.html)
+
+![image](./assets/head/kaxiwa.gif)
+
+---
+
+- [Blog](blog/index.html)
+- [Demo-egret-RichTextField](demo/egret-RichTextField/index.html)
+- [Demo-Song-Maker](demo/Song-Maker/index.html)
